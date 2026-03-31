21824fb9aef25972b44c8775fd61b1d38c68f91a
diegossilva-1995-01-25 <diego00023@gmail.com>
2026-03-31 17:05:34 -0300

    Criação de RestController de Conta e Transferências

--- transfer-schedule/src/main/java/br/com/diegossilva/app/controller/ContaRestController.java
+++ transfer-schedule/src/main/java/br/com/diegossilva/app/controller/ContaRestController.java
@@ -1,5 +1,58 @@
 package br.com.diegossilva.app.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.diegossilva.app.entity.Conta;
+import br.com.diegossilva.app.service.ContaService;
+
+@RestController
+@RequestMapping("/transf-sched/conta")
 public class ContaRestController {
+	
+	@Autowired
+	private ContaService service;
+	
+	@PostMapping("/cadastrar")
+	public ResponseEntity<String> cadastrarConta(Conta conta) {
+		
+		conta = service.cadastrarConta(conta);
+		
+		return ResponseEntity.ok().body("Conta cadastrada: " + conta.getId());
+		
+	}
+	
+	@PostMapping("/alterar/{id}")
+	public ResponseEntity<String> alterarConta(@PathVariable Integer id, @RequestBody Conta conta) {
+		
+		conta = service.alterarConta(id, conta);
+		
+		return ResponseEntity.ok().body("Conta alterada: " + conta.getId());
+		
+	}
+	
+	@PostMapping("/excluir/{id}")
+    public ResponseEntity<String> excluirConta(@PathVariable Integer id) {
+        service.excluirConta(id);
+        return ResponseEntity.ok().body("Conta excluída com sucesso: " + id);
+    }
+	
+	@GetMapping("/{id}")
+    public ResponseEntity<Conta> resgateBeneficiarioPorId(@PathVariable Integer id){
+        return ResponseEntity.ok(service.resgateContaPorId(id));
+    }
+    
+    @GetMapping("/todos")
+    public ResponseEntity<List<Conta>> resgateTodosBeneficiarios(){
+        return ResponseEntity.ok(service.resgateTodosContas());
+    }
 
 }
--- transfer-schedule/src/main/java/br/com/diegossilva/app/controller/TransferenciaRestController.java
+++ transfer-schedule/src/main/java/br/com/diegossilva/app/controller/TransferenciaRestController.java
@@ -1,5 +1,51 @@
 package br.com.diegossilva.app.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.diegossilva.app.entity.Transferencia;
+import br.com.diegossilva.app.service.TransferenciaService;
+
+@RestController
+@RequestMapping("/transf-sched/transf")
 public class TransferenciaRestController {
+	
+	@Autowired
+	private TransferenciaService service;
+	
+	@GetMapping("/todos-origem/{id}")
+	public ResponseEntity<List<Transferencia>> capturarTodasTransferenciasPorOrigem(@PathVariable Integer idOrigem) {
+		return ResponseEntity.ok(service.transferenciasPorOrigem(idOrigem));
+	}
+	
+	@GetMapping("/todos-destino/{id}")
+	public ResponseEntity<List<Transferencia>> capturarTodasTransferenciasPorDestino(@PathVariable Integer idDestino) {
+		return ResponseEntity.ok(service.transferenciasPorDestino(idDestino));
+	}
+	
+	@PostMapping("/{id}/agendar")
+	public ResponseEntity<Transferencia> agendarTransferencia(@RequestBody Transferencia transferencia) {
+		return ResponseEntity.ok(service.agendarTransferencia(transferencia));
+	}
+	
+	/*
+	@PostMapping("/{id}/alterar")
+	public ResponseEntity<Transferencia> alterarTransferencia(@RequestBody Transferencia transferencia) {
+		return ResponseEntity.ok(service.agendarTransferencia(transferencia));
+	}
+	
+	@PostMapping("/{id}/agendar")
+	public ResponseEntity<Transferencia> cancelarTransferencia(@RequestBody Transferencia transferencia) {
+		return ResponseEntity.ok(service.agendarTransferencia(transferencia));
+	}
+	*/
 
 }
